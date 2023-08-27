c60d0fbca5f110f866ba0cc06c2753ffc53da252
HashemDeveloper <juniayulia@outlook.com>
2023-08-26 21:15:37 -0400

    Implements splash screen

--- app/src/main/java/com/jrsoft/jetpack/tutorial/multimoduleapp/MainActivity.kt
+++ app/src/main/java/com/jrsoft/jetpack/tutorial/multimoduleapp/MainActivity.kt
@@ -6,29 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jrsoft.jetpack.tutorial.multimoduleapp.ui.theme.MultiModuleAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
-           EntryPoint()
+            EntryPoint()
         }
     }
 }
+
 @Composable
 private fun EntryPoint() {
     MultiModuleAppTheme {
         // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            color = MaterialTheme.colorScheme.background,
         ) {
-
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,4 +16,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "MultiModuleApp"
 include(":app")
- 
