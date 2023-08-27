98cbae2b646e03291a55ee1481a944879420b749
HashemDeveloper <juniayulia@outlook.com>
2023-08-27 13:26:20 -0400

    Adds the google auth client id

---
+++ app/src/main/java/com/jrsoft/jetpack/tutorial/multimoduleapp/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.jrsoft.jetpack.tutorial.multimoduleapp.utils
+
+object Constants {
+    const val MONGO_DB_APP_ID = "multimoduleapp-wlxze"
+    const val GOOGLE_AUTH_CLIENT_ID = "22890170244-he7ii8q4euu519p8rk0tgrce07llt358.apps.googleusercontent.com"
+}
