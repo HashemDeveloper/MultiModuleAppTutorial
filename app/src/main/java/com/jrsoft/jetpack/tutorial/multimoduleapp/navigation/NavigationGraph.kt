b0e920cc29bb84ef7cc24beaaee533522e466ffe
HashemDeveloper <juniayulia@outlook.com>
2023-08-27 23:39:13 -0400

    Displays dialog before signing out the user

--- app/src/main/java/com/jrsoft/jetpack/tutorial/multimoduleapp/navigation/NavigationGraph.kt
+++ app/src/main/java/com/jrsoft/jetpack/tutorial/multimoduleapp/navigation/NavigationGraph.kt
@@ -1,15 +1,14 @@
 package com.jrsoft.jetpack.tutorial.multimoduleapp.navigation
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -17,6 +16,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.jrsoft.jetpack.tutorial.multimoduleapp.ui.theme.components.DisplayAlertDialog
 import com.jrsoft.jetpack.tutorial.multimoduleapp.ui.theme.screens.auth.AuthenticationScreen
 import com.jrsoft.jetpack.tutorial.multimoduleapp.ui.theme.screens.auth.AuthenticationViewModel
 import com.jrsoft.jetpack.tutorial.multimoduleapp.ui.theme.screens.home.HomeScreen
@@ -28,6 +28,7 @@ import com.stevdzasan.onetap.rememberOneTapSignInState
 import io.realm.kotlin.mongodb.App
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun SetupNavigationGraph(startDestination: String, navController: NavHostController) {
@@ -36,10 +37,13 @@ fun SetupNavigationGraph(startDestination: String, navController: NavHostControl
             navController.popBackStack()
             navController.navigate(Screen.Home.route)
         })
-        homeRoute(onMenuClicked = {}, navigateToWriteScreen = {
+        homeRoute(navigateToWriteScreen = {
             navController.navigate(Screen.Write.route)
+        }, navigateToAuth = {
+            navController.popBackStack()
+            navController.navigate(Screen.Authentication.route)
         })
-        writeRoute(onBackPressed = { navController.popBackStack()})
+        writeRoute(onBackPressed = { navController.popBackStack() })
     }
 }
 
@@ -77,9 +81,39 @@ fun NavGraphBuilder.authenticationRoute(navigateToHome: () -> Unit) {
     }
 }
 
-fun NavGraphBuilder.homeRoute(onMenuClicked: () -> Unit, navigateToWriteScreen: () -> Unit) {
+@OptIn(ExperimentalMaterial3Api::class)
+fun NavGraphBuilder.homeRoute(navigateToWriteScreen: () -> Unit, navigateToAuth: () -> Unit) {
     composable(route = Screen.Home.route) {
-        HomeScreen(onMenuClicked = onMenuClicked, navigateToWriteScreen = navigateToWriteScreen)
+
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        var isSignOutDialogOpened by remember { mutableStateOf(false) }
+        val scope = rememberCoroutineScope()
+        HomeScreen(
+            drawerState = drawerState,
+            onSignOutClicked = {
+                isSignOutDialogOpened = true
+            },
+            onMenuClicked = {
+                scope.launch {
+                    drawerState.open()
+                }
+            }, navigateToWriteScreen = navigateToWriteScreen
+        )
+
+        DisplayAlertDialog(
+            title = "Sign Out",
+            message = "Are you sure you want to sign out from google account?",
+            isDialogOpen = isSignOutDialogOpened,
+            onCloseDialog = { isSignOutDialogOpened = false },
+            onConfirmClicked = {
+                scope.launch(Dispatchers.IO) {
+                    val user = App.Companion.create(MONGO_DB_APP_ID).currentUser
+                    user?.logOut()
+                    withContext(Dispatchers.Main) {
+                        navigateToAuth.invoke()
+                    }
+                }
+            })
     }
 }
 
---
+++ app/src/main/java/com/jrsoft/jetpack/tutorial/multimoduleapp/ui/theme/components/AlertDialog.kt
@@ -0,0 +1,54 @@
+package com.jrsoft.jetpack.tutorial.multimoduleapp.ui.theme.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DisplayAlertDialog(
+    title: String,
+    message: String,
+    isDialogOpen: Boolean,
+    onCloseDialog: () -> Unit,
+    onConfirmClicked: () -> Unit
+) {
+    if (isDialogOpen) {
+        AlertDialog(
+            modifier = Modifier.padding(horizontal = 24.dp),
+            title = {
+                Text(
+                    text = title, fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+                    fontWeight = FontWeight.Bold
+                )
+            },
+            text = {
+                Text(
+                    text = message, fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                    fontWeight = FontWeight.Normal
+                )
+            },
+            onDismissRequest = onCloseDialog,
+            confirmButton = {
+                Button(onClick = {
+                    onConfirmClicked.invoke()
+                    onCloseDialog.invoke()
+                }) {
+                    Text(text = "Yes")
+                }
+            },
+            dismissButton = {
+                OutlinedButton(onClick = onCloseDialog) {
+                    Text(text = "No")
+                }
+            }
+        )
+    }
+
+}
